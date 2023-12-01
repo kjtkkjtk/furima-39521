1016f0d9c6019d225132fa0a02ab5edc0ab6280f
kjtkkjtk <kadidas7726@gmail.com>
2023-12-01 17:49:21 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order(created_at: :desc)
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   
   private
 
